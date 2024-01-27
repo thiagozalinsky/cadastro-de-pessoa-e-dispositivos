42080f0469c1a755c0770b1063f81a1b3e49ce7a
thiagozalinsky <121072112+thiagozalinsky@users.noreply.github.com>
2024-01-26 22:10:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 <script>
-        function enviarDados() {
-            // Obter os dados do formulário
-            var formData = new FormData(document.getElementById('cadastroForm'));
+    function enviarDados() {
+        // Obter os dados do formulário
+        var formData = new FormData(document.getElementById('cadastroForm'));
 
-            // Enviar os dados para a API do SheetDB usando Fetch
-            fetch('https://sheetdb.io/api/v1/02ipcqrt0jjl7', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(Object.fromEntries(formData)),
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Dados enviados com sucesso:', data);
-                alert('Dados enviados com sucesso!');
-            })
-            .catch(error => {
-                console.error('Erro ao enviar dados:', error);
-                alert('Erro ao enviar dados. Por favor, tente novamente.');
-            });
-        }
-    </script>
+        // Criar um objeto com os dados do FormData
+        var formDataObject = {};
+        formData.forEach(function(value, key){
+            formDataObject[key] = value;
+        });
+
+        // Enviar os dados para a API do SheetDB usando Fetch
+        fetch('https://sheetdb.io/api/v1/02ipcqrt0jjl7', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formDataObject),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Dados enviados com sucesso:', data);
+            alert('Dados enviados com sucesso!');
+        })
+        .catch(error => {
+            console.error('Erro ao enviar dados:', error);
+            alert('Erro ao enviar dados. Por favor, tente novamente.');
+        });
+    }
+</script>
